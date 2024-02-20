cb18b8284e19f158d91313ef2b5aefd76f073002
Your Name <you@example.com>
2024-02-20 13:00:54 +0100

    Input sudoku

--- main/src/App.java
+++ main/src/App.java
@@ -20,15 +20,35 @@ public class App {
             if (i != 8) {
                 tabella += "\n\n||";
             }
-            
+
         }
-        System.out.println(tabella);
+        // System.out.println(tabella);
+        in.close();
 
+        prendiInput(coordinate);
 
+    }
 
-
-
-
+    public static void prendiInput(int[][] cor) {
+        Scanner in = new Scanner(System.in);
+        try {
+            int rig, col, num;
+            System.out.print("\nin che riga vuoi inserire il numero? ");
+            rig = in.nextInt();
+            System.out.print("\nin che colonna vuoi inserire il numero: ");
+            col = in.nextInt();
+            System.out.print("che numero vuoi inserire (1-9): ");
+            num = in.nextInt();
+            for (int i = 0; i < cor.length; i++) {
+                for (int j = 0; j < cor.length; j++) {
+                    if (i == rig - 1 && j == col - 1) {
+                        cor[i][j] = num;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // SCRIVI QUALCOSA
+        }
         in.close();
     }
 }
