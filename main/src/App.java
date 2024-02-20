096715e7282625e2f24bda51aa6db683d8951988
Your Name <you@example.com>
2024-02-20 13:04:58 +0100

    Stampa Sudoku

--- main/src/App.java
+++ main/src/App.java
@@ -12,23 +12,40 @@ public class App {
             }
             for (int j = 0; j < coordinate.length; j++) {
                 if (j == 2 || j == 5 || j == 8) {
-                    tabella += "  ||";
+                    tabella += " "  +coordinate[i][j]+ " ||";
                 } else {
-                    tabella += "  |";
+                    tabella += " " + coordinate[i][j] + " |";
                 }
             }
-            if (i != 8) {
-                tabella += "\n\n||";
-            }
-            
-        }
-        System.out.println(tabella);
 
+            if (i != 8){
+                if(i == 2 || i == 5){
+                    tabella += "\n\n\n||";
+                }else {
+                    tabella += "\n\n||";
+                }
+            }
+        }
+
+            
+    
+        //System.out.println(tabella);
+
+        stampaSoduko(coordinate, tabella);
 
 
 
 
 
         in.close();
+}
+
+    public static void stampaSoduko(int [][] cor, String tab){
+        System.out.println("\nHai completato il sudoku");
+        for (int i = 0; i < cor.length; i++) {
+            for (int j = 0; j < cor.length; j++) {    
+            }
+
+        } System.out.println(tab);
     }
 }
