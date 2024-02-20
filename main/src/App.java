d3f09a15379dc62d0e47a609a1e864d1514d70f4
Your Name <you@example.com>
2024-02-20 11:30:06 +0100

    Scheletro commit

--- main/src/App.java
+++ main/src/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+
+        int coordinate[][] = new int[9][9];
+        String tabella = "";
+        for (int i = 0; i < coordinate.length; i++) {
+            if (i == 0) {
+                tabella += "||";
+            }
+            for (int j = 0; j < coordinate.length; j++) {
+                if (j == 2 || j == 5 || j == 8) {
+                    tabella += "  ||";
+                } else {
+                    tabella += "  |";
+                }
+            }
+            if (i != 8) {
+                tabella += "\n\n||";
+            }
+            
+        }
+        System.out.println(tabella);
+
+
+
+
+
+
+        in.close();
     }
 }
